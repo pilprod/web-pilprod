5820749dba97587fc12e4eb9bb6ce5bfeeae166c
PILPROD <popov.pilprod@gmail.com>
2021-05-03 12:52:27 +0300

    Update Test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  // Start using Webpack 5
-  future: {
-    webpack5: true,
-  },
-  generateBuildId: async () => {
-    // You can, for example, get the latest git commit hash here
-    return process.env.NEXT_PUBLIC_PROJECT_NAME
-  },
+    // Using Webpack 5
+    future: {
+        webpack5: true,
+    },
+    generateBuildId: async() => {
+        // You can, for example, get the latest git commit hash here
+        return process.env.NEXT_PUBLIC_PROJECT_NAME
+    },
 }
